edf67718825bdb9c572abedd82d4f325ef5e2fb2
VitaliiBobylov <bobylevvitalyanatolievich@gmail.com>
2025-08-30 12:50:52 +0300

    jjjjj

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import css from "./App.module.css";
-import CafeInfo from "../CafeInfo/drgdr";
+import CafeInfo from "../CafeInfo/CafeInfo";
 import { useState } from "react";
 import type { Votes, VoteType } from "../../types/votes";
 import VoteOption from "../VoteOption/VoteOption";
